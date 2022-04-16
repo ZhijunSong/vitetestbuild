91665e41feca44d9238c42d7a803abbfc58b3a0a
Zhijun Song <songzhijun2015@gmail.com>
2022-04-16 00:43:45 -0600

    update app

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,80 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+   <div class="wrapper">
+    <!-- <canvas ref="myCanvas"></canvas>   -->
+    <a-scene
+      vr-mode-ui="enabled: false"
+      arjs="sourceType: webcam; videoTexture: true; debugUIEnabled: false;"
+    >
+      <a-entity
+        sp-aframe=" "
+        position="0 -2 0"
+        scale="0.8 0.8 0.8"
+        look-at="[gps-projected-camera]"
+      ></a-entity>
+      <a-camera fov="40" gps-projected-camera rotation-reader> </a-camera>
+
+    </a-scene>
+    <UI />
+   
+   </div>
+
+
 </template>
+<script>
+import {sculptToMinimalRenderer,createSculptureWithGeometry} from 'shader-park-core';
+import { nextTick } from 'vue'
+import {spCode} from './spCode.js';
+import UI from './components/ui.vue';
+
+export default {
+  components:{
+    UI
+  },
+  async mounted() {
+    await nextTick();
+
+  // let canvas = this.$refs.myCanvas;
+  //  sculptToMinimalRenderer(canvas, spCode);
+  },
+
+  methods:{
+    shaderShell(){
+        AFRAME.registerComponent("sp-aframe", {
+        init: function () {
+          this.geometry = new THREE.SphereGeometry(2, 2, 2);
+          this.params = {
+            time: 0.0,
+          };
+          let mesh = createSculptureWithGeometry(this.geometry, spCode, () => ({
+            time: this.params.time,
+          }));
+          this.material = new THREE.MeshStandardMaterial();
+          this.mesh = new THREE.Mesh(this.geometry, this.material);
+          this.mesh.material = mesh.material;
+          this.mesh.onBeforeRender = mesh.onBeforeRender;
+          this.el.setObject3D("mesh", this.mesh);
+          // this.el.object3D.scale.set(0.1,0.1,0.1);
+        },
+        tick: function (time, timeDelta) {
+          this.params.time += 0.01;
+        },
+      });
+      }
+  },
+  created: function() {
+        // this.getLocation();
+        this.shaderShell();
+
+  }
+
+}
+// const count = ref(0)
+</script>
+
+
+
+
 
 <style>
 #app {
@@ -18,4 +85,71 @@ import HelloWorld from './components/HelloWorld.vue'
   color: #2c3e50;
   margin-top: 60px;
 }
+.ui{
+  z-index: 999;
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+button#volumecontrol{
+    border: none;
+    background: none;
+    display: block;
+    position:absolute;
+    right:20px;
+    bottom: 10px;
+    color:white;
+}
+#location{
+   position:absolute;
+   left: 10px;
+   color:white;
+   font-weight: 600;
+   font-size: 12px;
+   top:10px;
+}    
+.ui{
+  display: block;
+  z-index: 1;
+  width: 100%;
+  height:100%;
+
+}
+video{
+  z-index:-999;
+  overflow: hidden;
+  visibility: hidden;
+}
+.arjs-loader {
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: none;
+  z-index: 9999;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.arjs-loader div {
+  text-align: center;
+  font-size: 1.25em;
+  color: white;
+}
+
+  
 </style>
---
+++ src/components/ui.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="ui">
+    <li id="location"><p>lat:{{lat}}, long:{{lon}}</p></li>
+    <button id="volumecontrol" @click="onoff">
+      <span v-if="on" class="material-icons">volume_up</span>
+      <span v-if="!on" class="material-icons">volume_off</span>
+    </button>
+    </div>
+</template>
+<script type="module">
+export default{
+  name:'UI',
+  data(){
+    return{
+     on: true,
+     lat:0,
+     lon:0, 
+     sound: null,
+     description: null,
+     fileid: null, 
+    }
+  },
+  methods:{
+    onoff(){
+      this.on =!this.on;
+      console.log(this.on);
+    },
+    getLocation(){
+      if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(
+          position=>{
+            this.lat=Math.round(position.coords.latitude * 100) / 100;
+            this.lon=Math.round(position.coords.longitude*100)/100;
+          }
+        );
+      }
+    },
+  },
+  created:function(){
+    this.getLocation();
+  }
+}
+
+</script>
+
+<style>
+
+
+body {
+  font-family: helvetica, arial, sans-serif;
+  margin: 2em;
+  width: 100vw; 
+  height: 100vh; 
+  margin : 0px; 
+  padding : 0px; 
+  border : 0px; 
+  background-color : white;
+}
+
+h1 {
+  font-style: italic;
+  color: #373fff;
+}
+
+canvas {
+  width: 100%;
+  height: 100%;
+  margin : 0px;
+  padding : 0px;
+  border : 0px;
+  background-color : transparent;
+}  
+  
+</style>
