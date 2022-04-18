6d0642e881dbe49caa7430ba5ad530066f93bb0a
Zhijun Song <songzhijun2015@gmail.com>
2022-04-17 23:12:25 -0600

    add vue router module

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+const path =require('path');
 // https://vitejs.dev/config/
 export default defineConfig({
+  alias:{
+    '@':path.resolve(__dirname,'./src' )
+  },
   base:'/vitetestbuild/',
   plugins: [vue()]
 })
