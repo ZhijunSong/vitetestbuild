1a5e567d1c2444e4743621601992f50ceac466ac
Zhijun Song <songzhijun2015@gmail.com>
2022-04-20 18:31:57 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
       '@':path.resolve(__dirname,'./src' )
     },
   },
-  publicPath: '/vitetestbuild/',
+  // publicPath: '/vitetestbuild/',
   base:'/vitetestbuild/',
   plugins: [
     vue(),
